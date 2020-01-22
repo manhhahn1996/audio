e88e0afe2788dba1936c38d0f73a55a18eeb1e02
NguyenKhanhHien <manhhabahn1996@gmail.com>
2020-01-22 14:55:35 +0700

    logic

--- play.js
+++ play.js
@@ -10,31 +10,20 @@ $(document).ready(function() {
         } else {
             $('.audio audio')[0].play();
         }
-        let mediaElement = $('.audio audio')[0];
-        // mediaElement.seekable.start(); // Returns the starting time (in seconds)
-        // mediaElement.seekable.end(); // Returns the ending time (in seconds)
-        // mediaElement.currentTime = mediaElement.duration - 20; // Seek to 122 seconds
-        // mediaElement.played.end();
-        console.log(mediaElement.duration)
     });
 
     $(document).on('click', '.btn-audio.volume', function() {
         $('.volume-bar').toggleClass('display')
     });
 
-    $('.progress-bar').attr('max', Math.floor($('.audio audio')[0].duration))
-
-    let speeds = [1.0, 0.5, 0.8, 1.2, 1.5]
+    let speeds = [1.0, 0.5, 0.8, 1.2, 1.5];
     let audio = $('.audio audio')[0];
-    let play = $('.btn-audio.play-pause');
-    let rewind = $('.pcast-rewind');
-    let forward = $('.pcast-forward');
-    let progress = $('.progress-bar');
+    let progress = $('#music-progress-bar');
     let speed = $('.btn-audio.speed');
     let mute = $('.pcast-mute');
     let currentTime = $('.capacity .start');
     let duration = $('.capacity .end');
-    let VolumeSlider = $('.volume-bar');
+    let VolumeSlider = $('#volume-bar');
 
     let currentSpeedIdx = 0;
 
@@ -48,10 +37,10 @@ $(document).ready(function() {
 
         let time = minutes + ':' + seconds;
         return time;
-    }
+    };
 
     function onloadedmetadata() {
-        // progress.setAttribute('max', Math.floor(audio.duration));
+        progress.attr('aria-valuemax', Math.floor(audio.duration));
         duration.text(toHHMMSS(audio.duration));
     }
     if (audio.duration) {
@@ -61,21 +50,18 @@ $(document).ready(function() {
     }
     audio.addEventListener('timeupdate', function() {
         progress.css('width', audio.currentTime / audio.duration * 100 + '%');
+        progress.attr('aria-valuenow', audio.currentTime);
         currentTime.text(toHHMMSS(audio.currentTime));
+        if (audio.currentTime === audio.duration) {
+            $('.fa-play').removeClass('not-display');
+            $('.fa-pause').removeClass('display');
+        }
     });
 
-    rewind.on('click', function() {
-        audio.currentTime -= 3;
-    }, false);
+    $(document).on('click', '#music-progress', function(e) {
+        audio.currentTime = Math.floor(audio.duration) * (e.offsetX / $('#music-progress').width());
 
-    forward.on('click', function() {
-        audio.currentTime += 3;
-    }, false);
-
-    progress.on('click', function(e) {
-        //TODO div onclick
-        audio.currentTime = Math.floor(audio.duration) * (e.offsetX / e.target.offsetWidth);
-    }, false);
+    });
 
     speed.on('click', function() {
         currentSpeedIdx = currentSpeedIdx + 1 < speeds.length ? currentSpeedIdx + 1 : 0;
@@ -84,12 +70,8 @@ $(document).ready(function() {
     });
 
     VolumeSlider.change('input', function() {
-        audio.volume = parseInt(this.value) / 10;
-        step = .01;
-        min = 0;
-        max = 1;
-        value = 1;
-    }, false);
+        audio.volume = parseInt(this.value) / 100;
+    });
 
     mute.on('click', function() {
         if (audio.muted) {
@@ -103,7 +85,6 @@ $(document).ready(function() {
 
     $(document).on('click', '.btn-audio.repeat', function() {
         let audio = $('.audio audio')[0];
-        // $('.btn-audio.repeat').toggleClass('audio-background');
         if ($('.btn-audio.repeat-time').hasClass('audio-background')) {
             audio.currentTime = audio.currentTime - 3;
         } else {
@@ -112,19 +93,7 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '.btn-audio.repeat-time', function() {
-        let audio = $('.audio audio')[0];
         $('.btn-audio.repeat-time').toggleClass('audio-background');
     });
 
-    $(document).on('click', '.btn-audio.download', function() {
-        let audio = $('.audio audio')[0];
-        // $('.btn-audio.download').attr('href', audio.src);
-        $('.btn-audio.download').attr('download', 'filename.mp3');
-    });
-
-    $(document).on('click', '.btn-audio.speed', function() {
-        // let audio = $('.audio audio')[0];
-        // console.log($('.speed').text())
-        // audio.playbackRate = 1.2;
-    });
 });
